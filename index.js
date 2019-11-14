5601b1a49e2b70b87be66d04e874926c6aa75a3c
Roman <r.gutsu@bk.ru>
2019-11-14 15:28:54 +0200

    home-work

---
+++ index.js
@@ -0,0 +1,38 @@
+alert("Стандартный вид уравнения: \n aх²+bx+c=0");
+var a = parseInt(prompt("Введите a",1));
+var b = parseInt(prompt("Vvedite b", 1));
+var c = parseInt(prompt("Vvedite c", 1));
+alert(a+"х²+"+b+"x+"+c+"=0");
+function Mat(a,b,c){
+ return b*b - 4*a*c
+}
+var D = Mat(a,b,c);
+function first(a,b,c){ var one = -b;
+var two = 2*a;
+return one / two 
+}
+var x = first(a,b,c);
+function sec(a,b,c){ var one = -b;
+var two = - Math.sqrt(D)
+var three = one + two ;
+var four = 2*a;
+return three / four
+}
+var x1 = sec(a,b,c);
+function thr(a,b,c){ var one = -b;
+var two = Math.sqrt(D)
+var three = one + two ;
+var four = 2*a;
+return three / four
+}
+var x2 = thr(a,b,c);
+var result;
+if (D < 0){alert("Net kornea")} else {if(D === 0){result = "x="+x;}else {result="x1="+x1+" x2="+x2;}}
+if (D === 0 || D > 0){alert(result);};
+var resultText = "<h2>"+"Ваш ответ:"+"<br>"+result+"</h2>";
+var example = "<h2>"+"Стандартный вид уравнения:"+"<br>"+"aх²+bx+c=0"+"</h2>";
+var yourExample ="<h2>"+"Ваше уравнение:"+"<br>"+a+"х²+"+b+"x+"+c+"=0"+"</h2>";
+var divElement = document.createElement('div');
+divElement.innerHTML = example + yourExample + resultText;
+document.body.style.fontWeight = "bold";
+document.body.append(divElement);
